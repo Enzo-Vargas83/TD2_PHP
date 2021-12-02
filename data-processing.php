359bffb9a4eff13218b0d6463e50484663cfe3b0
Vargas Enzo <enzo.vargas@etu.univ-amu.fr>
2021-12-02 17:13:15 +0100

    ajout du form

--- data-processing.php
+++ data-processing.php
@@ -1,28 +1,23 @@
 <?php
- if (!empty($_POST))
- {
-    $action = $_POST['action'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    if ($action == 'mailer') {
+ if (!empty($_POST)) {
+     $action = $_POST['action'];
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+     if ($action == 'mailer') {
 
+         $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
+         $message .= 'Email : ' . $email . PHP_EOL;
+         $message .= 'Mot de passe : ' . PHP_EOL . $password;
+         echo $message;
 
-        $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
-        $message .= 'Email : ' . $email . PHP_EOL;
-        $message .= 'Mot de passe : ' . PHP_EOL . $password;
-        echo $message;
-
-        $from = $email;
-        $to = "enzovargaspro83520@gmail.com";
-        $subject = "test";
-        $headers = "De :" . $from;
-        mail($to,$subject,$message, $headers);
-        echo "<br> L'email a été envoyé <br/>";
-
-    }
-    else echo '<br/><strong>Bouton non géré !</strong><br/>';
- else{
-
- }
+         $from = $email;
+         $to = "enzovargaspro83520@gmail.com";
+         $subject = "test";
+         $headers = "De :" . $from;
+         mail($to, $subject, $message, $headers);
+         echo "<br> L'email a été envoyé <br/>";
+     } else {
+         echo '<br/><strong>Bouton non géré !</strong><br/>';
+     }
  }
 ?>
