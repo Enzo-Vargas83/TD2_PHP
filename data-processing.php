7c9c3571166c8abea92b8b75c24863c2b6b59eb2
Vargas Enzo <enzo.vargas@etu.univ-amu.fr>
2021-12-02 15:30:50 +0100

    ajout du form

--- data-processing.php
+++ data-processing.php
@@ -2,8 +2,13 @@
 
 $action = $_POST['action'];
 $email = $_POST['email'];
+$password = $_POST['password'];
 if ($action == 'mailer') {
+
+    $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
     $message .= 'Email : ' . $email . PHP_EOL;
+    $message .= 'Mot de passe : ' . PHP_EOL . $password;
+
 }
 else {
     echo '<br/><strong>Bouton non géré !</strong><br/>';
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<?php
+include 'utils.inc.php';
+?>
+
 <form action="data-processing.php" method="post">
     <input type="text" name="identifiant"><br/>
     <input type="radio" name="sexe"><br/>
-    <div>
-        <label for="email">email </label>
-        <input type="text" name="email"><br/>
-    </div>
+    <label for="email">email </label>
+    <input type="text" name="email"><br/>
     <input type="password" name="password"><br/>
     <input type="password" name="verifpassword"><br/>
     <input type="text" name="numtel"><br/>
