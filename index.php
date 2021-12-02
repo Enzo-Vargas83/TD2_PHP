500d3b8774c1608233e147b5b08c4bc2208ae95a
Vargas Enzo <enzo.vargas@etu.univ-amu.fr>
2021-12-02 15:32:59 +0100

    ajout du form

--- index.php
+++ index.php
@@ -267,6 +267,6 @@ include 'utils.inc.php';
         <option value="Zimbabwe">Zimbabwe </option>
     </select>
     <input type="checkbox" name="condition géneral">
-    <input type="submit" name="mailer">
+    <input type="submit" name="mailer" action="mailer">
 
 </form>
