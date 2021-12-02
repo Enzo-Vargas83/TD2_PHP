f69f86c0fe20c8cc47fa28829d7a6bfc6e974ab9
Vargas Enzo <enzo.vargas@etu.univ-amu.fr>
2021-12-02 15:47:06 +0100

    ajout du form

--- data-processing.php
+++ data-processing.php
@@ -5,12 +5,15 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 if ($action == 'mailer') {
 
+
     $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
     $message .= 'Email : ' . $email . PHP_EOL;
     $message .= 'Mot de passe : ' . PHP_EOL . $password;
+    echo $message;
 
 }
 else {
     echo '<br/><strong>Bouton non géré !</strong><br/>';
+
 }
 ?>
