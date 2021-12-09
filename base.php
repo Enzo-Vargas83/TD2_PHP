e8cbde0d2e71f5a02e221f4debf752062af1343e
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 14:25:57 +0100

    base de donnée mise à jour

--- base.php
+++ base.php
@@ -1,6 +1,6 @@
 <?php
 
-    $dbLink = mysqli_connect(vargas_td2, vargas, lolo83520)
+    $dbLink = mysqli_connect(mysql-vargas.alwaysdata.net, vargas, lolo83520)
     or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 
     mysqli_select_db($dbLink , user) or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
